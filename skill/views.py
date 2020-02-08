09b9cda4c472d92b3397ffc854fb7ffc8e705f6c
Sungjin Hong <rapportyou@gmail.com>
2020-02-09 01:16:47 +0900

    init

---
+++ skill/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+
+#from .models import Question
+
+
+def index(request):
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('skill/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
